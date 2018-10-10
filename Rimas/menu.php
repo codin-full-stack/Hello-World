4d2932ccce0b7517748c01beb726fdf0e968a0a3
Rimas <42798462+Rimas9111@users.noreply.github.com>
2018-10-10 16:07:01 +0300

    frontend to php in good progress

--- Rimas/menu.php
+++ Rimas/menu.php
@@ -1,10 +1,17 @@
-<div class="menu">
-<a href="posts.php">News</a>
-<a href="register.php">Registration</a>
-<a href="loginDB.php">Login</a>
-<a href="search.php">Search</a>
-<a href="users.php">Users</a>
-<?php
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Rimas</title>
+</head>
 
-?>
+<div class="menu">
+    <a href="posts.php">News</a>
+    <a href="register.php">Registration</a>
+    <a href="loginDB.php">Login</a>
+    <a href="search.php">Search</a>
+    <a href="users.php">Users</a>
 </div>
+</html>
--- Rimas/users.php
+++ Rimas/users.php
@@ -1,70 +1,33 @@
-<?php include "header.php"; ?>
-<head>
-    <link rel="stylesheet" href="style.css">
-</head>
-<div class="userBoard">
-
-<?php
-
-// MySqL, atvaizduoja duomenis
-
-$sql = "SELECT * FROM  users";
-// padaro masyva???
+<?php 
+include "header.php";
+$sql = "SELECT first_name, last_name, age, city, email FROM users";
 $result = mysqli_query($connection, $sql);
-// $result=$con->query($sql);
-// užklausos įvykdymas
-if($result = mysqli_query($connection, $sql)){
-   // užklausos grąžinimas
-   // grąţina iš duomenų bazės ištrauktų eilučių skaičių pagal užklausą
-   if(mysqli_num_rows($result) > 0){
-       echo "<table>";
-       echo "<thead>";
-       echo "<tr>";
-               echo "<th>id</th>";
-               echo "<th>vardas</th>";
-               echo "<th>pavarde</th>";
-               echo "<th>amzius</th>";
-               echo "<th>lytis</th>";
-               echo "<th>miestas</th>";
-               echo "<th>el_pastas</th>";
-               echo "<th>naujienos</th>";
-               echo "<th>taisykles</th>";
-               echo "<th>slaptazodis</th>";
-           echo "</tr>";
-       echo "</thead>";
-       echo "<tbody>";
-           // grąžinama viena EINAMOJI eilutė iš viso eilučių sąrašo
-       while($row = mysqli_fetch_array($result)){
-           echo "<tr>";
-               echo "<td>" . $row['id'] . "</td>";
-               echo "<td>" . $row['first_name'] . "</td>";
-               echo "<td>" . $row['last_name'] . "</td>";
-               echo "<td>" . $row['age'] . "</td>";
-               echo "<td>" . $row['gender'] . "</td>";
-               echo "<td>" . $row['city'] . "</td>";
-               echo "<td>" . $row['email'] . "</td>";
-               echo "<td>" . $row['news'] . "</td>";
-               echo "<td>" . $row['rules'] . "</td>";
-               echo "<td>" . $row['password'] . "</td>";
-
-           echo "</tr>";
-       }
-
-       echo "</tbody>";
-
-       echo "</table>";
-       // Free result set
-       mysqli_free_result($result);
-   } else{
-       echo "No records matching your query were found.";
-   }
-} else {
-   echo "ERROR $sql. " . mysqli_error($connection);
-}
-
-
 ?>
-
-<br> <br>
-<input class="mygtukas" style="border: none" type="button" onclick="location.href = 'index.php';" value="Home" />
+<div class="usertable">
+<div style="overflow-x:auto;">
+<table>
+    <thead>
+        <tr>
+            <th>Full name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Location</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php if (mysqli_num_rows($result) > 0) { ?>
+                <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                <tr>
+                    <td><?php echo $row['first_name'] . " " . $row['last_name']?></td>
+                    <td><?php echo $row['age']?></td>
+                    <td><?php echo $row['email']?></td>
+                    <td><?php echo $row['city']?></td>
+                    <?php } ?>
+                </tr>
+    <?php } else { ?>
+        <tr><td colspan="4">Rezultanu nera</td></tr>
+    <?php } ?>
+    </tbody>
+</table>
+    </div>
 </div>

fb259ab405ae6eac435c90d4f26b97a20dfe168a
Rimas <42798462+Rimas9111@users.noreply.github.com>
2018-10-24 14:30:51 +0300

    mightmedia
    iridium

--- mightmedia_cms-trunk/stiliai/iridium/index.php
+++ mightmedia_cms-trunk/stiliai/iridium/index.php
@@ -20,27 +20,31 @@
 				
 				<!-- Logo -->
 				<div id="logo">
-					<h1><a href="<?php echo adresas(); ?>">Iridium</a></h1>
-					<span>Design by TEMPLATED</span>
+					<h1><a href="<?php echo adresas(); ?>">
+						<?php echo input( strip_tags( $conf['Pavadinimas'] )); ?>
+					</a></h1>
+					<span>
+						<?php echo $conf['Apie']; ?>
+					</span>
 				</div>
 				
 				<!-- Nav -->
 				<nav id="nav">
-				<ul>
-					<?php
-						$limit = 6; //Kiek nuorodų rodome
-						$menuSql  = mysql_query1( "SELECT * FROM `" . LENTELES_PRIESAGA . "page` WHERE `parent` = 0 AND `show` = 'Y' AND `lang` = " . escape(lang()) . " ORDER BY `place` ASC LIMIT " . $limit );
-					?>
-					<?php foreach ($menuSql as $menuRow) { ?>
-						<?php if (teises( $menuRow['teises'], $_SESSION[SLAPTAS]['level'])){ ?>
-							<li>
-								<a href="<?php echo url('?id,' . (int)$menuRow['id']); ?>">
-									<?php echo input($menuRow['pavadinimas']); ?>
-								</a>
-							</li>
+					<ul>
+						<?php
+							$limit = 6; //Kiek nuorodų rodome
+							$menuSql  = mysql_query1( "SELECT * FROM `" . LENTELES_PRIESAGA . "page` WHERE `parent` = 0 AND `show` = 'Y' AND `lang` = " . escape(lang()) . " ORDER BY `place` ASC LIMIT " . $limit );
+						?>
+						<?php foreach ($menuSql as $menuRow) { ?>
+							<?php if (teises( $menuRow['teises'], $_SESSION[SLAPTAS]['level'])){ ?>
+								<li>
+									<a href="<?php echo url('?id,' . (int)$menuRow['id']); ?>">
+										<?php echo input($menuRow['pavadinimas']); ?>
+									</a>
+								</li>
+							<?php } ?>
 						<?php } ?>
-					<?php } ?>
-				</ul>
+					</ul>
 				</nav>
 			</div>
 		</div>
@@ -53,94 +57,18 @@
 					<!-- Content -->
 					<div id="content" class="8u skel-cell-important">
 						<?php
-						if ( isset( $strError ) && !empty( $strError ) ) {
-							klaida( "Klaida", $strError );
-						}
-
-						include ( "priedai/centro_blokai.php" );
-						include ( $page . ".php" );
+							if ( isset( $strError ) && !empty( $strError ) ) {
+								klaida( "Klaida", $strError );
+							}
+							
+							include ( "priedai/centro_blokai.php" );
+							include ( $page . ".php" );
 						?>
 					</div>
 					
 					<!-- Sidebar -->
 					<div id="sidebar" class="4u">
-						<section>
-							<header>
-								<h2>Pellentesque vulputate</h2>
-							</header>
-							<?php include ( "priedai/desines_blokai.php" ); ?>
-						</section>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Footer -->
-		<div id="featured">
-			<div class="container">
-				<div class="row">
-					<div class="4u">
-						<h2>Aenean elementum facilisis</h2>
-						<a href="#" class="image full"><img src="images/pic01.jpg" alt="" /></a>
-						<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum. Quisque dictum. Pellentesque viverra vulputate enim.</p>
-						<p><a href="#" class="button">More Details</a></p>
-					</div>
-					<div class="4u">
-						<h2>Fusce ultrices fringilla</h2>
-						<a href="#" class="image full"><img src="images/pic02.jpg" alt="" /></a>
-						<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum. Quisque dictum. Pellentesque viverra vulputate enim.</p>
-						<p><a href="#" class="button">More Details</a></p>
-					</div>
-					<div class="4u">
-						<h2>Etiam rhoncus volutpat erat</h2>
-						<a href="#" class="image full"><img src="images/pic03.jpg" alt="" /></a>
-						<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum. Quisque dictum. Pellentesque viverra vulputate enim.</p>
-						<p><a href="#" class="button">More Details</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-
-		<!-- Footer -->
-		<div id="footer">
-			<div class="container">
-				<div class="row">
-					<div class="4u">
-						<section>
-							<h2>Latest Posts</h2>
-							<ul class="default">
-								<li><a href="#">Pellentesque lectus gravida blandit</a></li>
-								<li><a href="#">Lorem ipsum consectetuer adipiscing</a></li>
-								<li><a href="#">Phasellus nibh pellentesque congue</a></li>
-								<li><a href="#">Cras vitae metus aliquam pharetra</a></li>
-								<li><a href="#">Maecenas vitae orci feugiat eleifend</a></li>
-							</ul>
-						</section>
-					</div>
-					<div class="4u">
-						<section>
-							<h2>Ultrices fringilla</h2>
-							<ul class="default">
-								<li><a href="#">Pellentesque lectus gravida blandit</a></li>
-								<li><a href="#">Lorem ipsum consectetuer adipiscing</a></li>
-								<li><a href="#">Phasellus nibh pellentesque congue</a></li>
-								<li><a href="#">Cras vitae metus aliquam pharetra</a></li>
-								<li><a href="#">Maecenas vitae orci feugiat eleifend</a></li>
-							</ul>
-						</section>
-					</div>
-					<div class="4u">
-						<section>
-							<h2>Aenean elementum</h2>
-							<ul class="default">
-								<li><a href="#">Pellentesque lectus gravida blandit</a></li>
-								<li><a href="#">Lorem ipsum consectetuer adipiscing</a></li>
-								<li><a href="#">Phasellus nibh pellentesque congue</a></li>
-								<li><a href="#">Cras vitae metus aliquam pharetra</a></li>
-								<li><a href="#">Maecenas vitae orci feugiat eleifend</a></li>
-							</ul>
-						</section>
+						<?php include ( "priedai/desines_blokai.php" ); ?>
 					</div>
 				</div>
 			</div>
@@ -149,6 +77,7 @@
 		<!-- Copyright -->
 		<div id="copyright">
 			<div class="container">
+				<?php copyright( $conf['Copyright'] );?>
 				Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
 			</div>
 		</div>
